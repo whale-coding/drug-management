04f934cf28f6930dce42060b5920b25b5d2a4c66
计科192-聂建强 <859193125@qq.com>
2021-07-22 21:03:25 +0800

    使用Shiro完成登录验证

--- src/main/java/com/study/drug/controller/UserController.java
+++ src/main/java/com/study/drug/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.study.drug.controller;
 
 import com.study.drug.common.ResultMapUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,8 +32,22 @@ public class UserController {
     @PostMapping("/login")
     @ResponseBody
     public Object login(String username,String password){
+        if (username==null||password==null){
+            return ResultMapUtil.getHashMapLogin("用户名和密码不能为空","2");
+        }
 
-        return ResultMapUtil.getHashMapLogin("验证成功","1");
+        //结合Shiro实现登录验证
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        try{
+            subject.login(token);
+        }catch (UnknownAccountException e){
+            //code:2  失败
+            return ResultMapUtil.getHashMapLogin("用户名不存在","2");
+        }catch (IncorrectCredentialsException e){
+            return ResultMapUtil.getHashMapLogin("密码错误","2");
+        }
+        return ResultMapUtil.getHashMapLogin("登录成功","1");
     }
 
     /**
