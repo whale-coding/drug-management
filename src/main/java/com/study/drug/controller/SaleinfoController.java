193fd24b004381806c995fca257d9d1aa0d50937
计科192-聂建强 <859193125@qq.com>
2021-07-23 11:23:42 +0800

    销售记录功能实现

---
+++ src/main/java/com/study/drug/controller/SaleinfoController.java
@@ -0,0 +1,129 @@
+package com.study.drug.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+
+import com.study.drug.common.ResultMapUtil;
+import com.study.drug.pojo.Saleinfo;
+import com.study.drug.service.SaleinfoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * 销售记录相关的controller
+ */
+@Controller
+@RequestMapping(value = "/saleinfo")
+public class SaleinfoController {
+
+    @Resource
+    private SaleinfoService saleinfoService;
+
+    /**
+     * 转向销售记录页面
+     */
+    @RequestMapping
+    public String saleinfo(){
+        return "saleinfo";
+    }
+
+    /**
+     * 分页查询销售记录列表
+     */
+    @RequestMapping(value = "/saleinfoQueryPage")
+    @ResponseBody
+    public Object saleinfoQueryPage(String param, @RequestParam(defaultValue = "1")int page,@RequestParam(defaultValue = "10")int limit){
+        try{
+            IPage<Saleinfo> iPage = saleinfoService.selectSaleinfoPage(page,limit,param);
+            return ResultMapUtil.getHashMapMysqlPage(iPage);
+        } catch (Exception e){
+            return ResultMapUtil.getHashMapException(e);
+        }
+    }
+
+    /**
+     * 转向销售记录新增页面
+     */
+    @RequestMapping(value = "/saleinfoPage")
+    public String saleinfoPage(){
+        return "saleinfoPage";
+    }
+
+    /**
+     * 添加一个销售记录
+     */
+    @RequestMapping(value = "/saleinfoAdd")
+    @ResponseBody
+    public Object saleinfoAdd(Saleinfo saleinfo){
+        try{
+            int i = saleinfoService.addSaleinfo(saleinfo);
+            return ResultMapUtil.getHashMapSave(i);
+        } catch (Exception e){
+            return ResultMapUtil.getHashMapException(e);
+        }
+    }
+
+    /**
+     * 转向销售记录编辑页面
+     */
+    @RequestMapping(value = "/saleinfoQueryById")
+    public String saleinfoQueryById(@RequestParam(name = "id",required = true)Integer id, Model model){
+        Saleinfo saleinfo = saleinfoService.querySaleinfoById(id);
+        model.addAttribute("obj",saleinfo);
+        return "saleinfoPage";
+    }
+
+    /**
+     * 修改一个销售记录
+     */
+    @RequestMapping(value = "/saleinfoEdit")
+    @ResponseBody
+    public Object saleinfoEdit(Saleinfo saleinfo){
+        try{
+            int i = saleinfoService.editSaleinfo(saleinfo);
+            return ResultMapUtil.getHashMapSave(i);
+        } catch (Exception e){
+            return ResultMapUtil.getHashMapException(e);
+        }
+    }
+
+    /**
+     * 删除一个销售记录
+     */
+    @RequestMapping(value = "/saleinfoDelById")
+    @ResponseBody
+    public Object saleinfoDelById(Integer id){
+        try{
+            int i = saleinfoService.delSaleinfoById(id);
+            return ResultMapUtil.getHashMapDel(i);
+        } catch (Exception e){
+            return ResultMapUtil.getHashMapException(e);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/study/drug/mapper/SaleinfoMapper.java
@@ -0,0 +1,12 @@
+package com.study.drug.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.study.drug.pojo.Saleinfo;
+
+
+/**
+ * 药品销售记录的增删改查Mapper
+ */
+public interface SaleinfoMapper extends BaseMapper<Saleinfo> {
+
+}
