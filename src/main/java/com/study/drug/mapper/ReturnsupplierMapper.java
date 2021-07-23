a7c2920dec50e921ded84acba734117b3d7ee141
计科192-聂建强 <859193125@qq.com>
2021-07-23 11:45:36 +0800

    退货给供应商功能实现

---
+++ src/main/java/com/study/drug/mapper/ReturnsupplierMapper.java
@@ -0,0 +1,11 @@
+package com.study.drug.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.study.drug.pojo.Returnsupplier;
+
+/**
+ * 退货给供应商的增删改查Mapper
+ */
+public interface ReturnsupplierMapper extends BaseMapper<Returnsupplier> {
+
+}
